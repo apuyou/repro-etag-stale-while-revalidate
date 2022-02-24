74b989fb655ec358c6fd13c813c4056d8d3b0861
Arthur Puyou <arthur@puyou.fr>
2022-02-24 19:06:05 +0100

    Test on Vercel

--- test.mjs
+++ test.mjs
@@ -1,25 +1,28 @@
 import fetch from "node-fetch";
 
 const request = async (currentEtag) => {
-  const response = await fetch("http://localhost:3000/api/hello", {
-    headers: currentEtag
-      ? {
-          "if-none-match": currentEtag,
-        }
-      : undefined,
-  });
-  const result = await response.json();
+  const response = await fetch(
+    "https://repro-etag-stale-while-revalidate.vercel.app/api/hello",
+    {
+      headers: currentEtag
+        ? {
+            "if-none-match": currentEtag,
+          }
+        : undefined,
+    }
+  );
+  // const result = await response.json();
   const etag = response.headers.get("etag");
   console.log(
     `Result status=${
       response.status
-    } if-none-match=${currentEtag} etag=${etag} match=${
-      currentEtag === etag
-    } time=${result.time}`
+    } if-none-match=${currentEtag} etag=${etag}  x-vercel-cache=${response.headers.get(
+      "x-vercel-cache"
+    )}`
   );
 
   setTimeout(() => {
-    request(etag);
+    request(etag || currentEtag);
   }, 1000);
 };
 
