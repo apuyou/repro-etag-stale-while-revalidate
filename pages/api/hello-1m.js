5bb50d49408db003b2eaf73d13b821d77ea8a920
Arthur Puyou <arthur@puyou.fr>
2022-04-11 00:38:02 +0200

    Fix 1m

--- pages/api/hello-1m.js
+++ pages/api/hello-1m.js
@@ -2,7 +2,9 @@
 
 export default function handler(req, res) {
   res.setHeader("Cache-Control", "s-maxage=20, stale-while-revalidate");
-  res
-    .status(200)
-    .json({ name: "John Doe", time: (new Date().getTime() / 1000) % 60 });
+  const seconds = Math.floor(new Date().getTime() / 1000);
+  res.status(200).json({
+    name: "John Doe",
+    time: seconds - (seconds % 60),
+  });
 }
